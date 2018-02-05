01051a20796149c4d2a2aeb2f0a6f93ade65863d
vmstarchenko <vmstarchenko@edu.hse.ru>
2018-02-05 21:56:53 +0300

    add test images and fix absolute paths

--- 2/ascii/src/main.java
+++ 2/ascii/src/main.java
@@ -9,14 +9,10 @@ import java.util.HashMap;
 public final class main {
 
     public static void main(String[] args) {
-        File f = new File("/home/vladimir/tmp/img4.png");
+        File f = new File("input_images/digits.png");
 
         try {
             AsciiImage image = new AsciiImage(f, AsciiImage.default_palette);
-
-            // image = image.getRectangle(image.getWidth(), 0, 0,
-            // image.getHeight());
-            // image = image.downscale(100, 100);
 
             // split by lines and resize
             Vector<AsciiImage> subs = image.splitByThreshold(0.95);
@@ -27,8 +23,7 @@ public final class main {
                     continue;
                 cur = cur.resize(50, 50);
 
-                String filename =
-                    "/home/vladimir/tmp/digits/" + real_i + ".png";
+                String filename = "output_images/digits/" + real_i + ".png";
 
                 System.out.println(filename);
                 System.out.println(AsciiImage.distance(cur, cur));
@@ -43,26 +38,24 @@ public final class main {
 
             // print as image
             // BufferedImage realImage = image.resize(50, 50).convertToImage();
-            BufferedImage realImage = image.scale(.5).convertToImage();
-            ImageIO.write(realImage, "png",
-                          new File("/home/vladimir/tmp/img5.png"));
+            BufferedImage realImage = image.scale(.5).invert().convertToImage();
+            ImageIO.write(realImage, "png", new File("output_images/img5.png"));
 
             // print full ascii image
             final String ascii = image.resize(100, 100).invert().getAsciiImage();
             System.out.println(ascii);
 
-            /*
+
             Predictor p = new Predictor(map);
 
             AsciiImage pimage =
-                new AsciiImage(new File("/home/vladimir/tmp/img9.jpg"),
-                               AsciiImage.default_palette)
+                new AsciiImage(new File("input_images/digit_test.jpg"), AsciiImage.default_palette)
                     .resize(50, 50);
 
             int value = p.predict(pimage);
-            System.out.println(value);
+
             System.out.println(pimage.getAsciiImage());
-            */
+            System.out.println("Predicted value: " + value);
 
             System.exit(0);
         } catch (IOException e) {
